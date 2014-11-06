47aa4f3becfec39081a5c17cb2c6084fea8f6432
Blaz Repas <info@bss.si>
2014-11-06 16:06:33 +0100

    Basic tests for exception handling

---
+++ test/Examples/Exception.hs
@@ -0,0 +1,35 @@
+module Examples.Exception where
+
+import Control.Effects
+import Control.Effects.Exception
+
+handleException = runEff . handle exceptionHandler
+
+-------------------------------------------------------
+
+prg1 = throw "something went wrong"
+
+prg1res :: Either String a
+prg1res = handleException prg1
+
+-------------------------------------------------------
+
+prg2 n = return (n + 1)
+
+prg2res :: Integer -> Either String Integer
+prg2res n = handleException  $ prg2 n
+
+-------------------------------------------------------
+
+prg3 = do
+  v <- ask
+  if v < 15 
+  then throw $ show v
+  else return (v+1)
+
+prg3run n = runEff 
+                 . handle exceptionHandler 
+                 . handle (readerHandler n)
+
+prg3res :: Integer -> Either String Integer
+prg3res n = prg3run n prg3
--- test/Main.hs
+++ test/Main.hs
@@ -2,13 +2,24 @@ module Main where
 
 import Test.Hspec
 import Test.QuickCheck
+import Data.Either (isLeft)
 
 import Examples.Reader
 import Examples.Misc
+import qualified Examples.Exception as Exc
 
 main :: IO ()
 main = hspec $ do
   describe "A reader handler" $ do
     it "Returns the same value with simple ask" $ 
       property $ \x -> prgReader1res x == x
+
+  describe "An exception handler" $ do
+    it "Retruns a Left value if an exception was thrown" $
+      (isLeft Exc.prg1res) `shouldBe` True
+
+    it "Returns a Right value if no exeption was thrown" $
+      property $ \x -> Exc.prg2res x == (Right $ x + 1)
+      
+
 
