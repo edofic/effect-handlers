404c9a7db721c7c711a0c55525d0ffd0e5a27181
Andraz Bajt <edofic@gmail.com>
2014-11-06 14:33:25 +0100

    lift io effect for issue #8

---
+++ src/Control/Effects/IO.hs
@@ -0,0 +1,18 @@
+module Control.Effects.IO where 
+
+import Control.Effects
+import Data.Typeable
+
+data LiftIO a = forall r . LiftIO (IO r) (r -> a) deriving (Typeable)
+
+instance Functor LiftIO where
+  fmap f (LiftIO c k) = LiftIO c (f . k)
+
+liftIO :: Member LiftIO r => IO a -> Eff r a
+liftIO c = effect $ LiftIO c return
+
+ioHandler :: Handler LiftIO '[] a (IO a)
+ioHandler (Left a) = 
+  return $ return a
+ioHandler (Right (LiftIO c k)) = 
+  return $ c >>= (runEff . k)
