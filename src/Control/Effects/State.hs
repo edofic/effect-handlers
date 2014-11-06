0e6e5af929ea8991ac3022aa6c7c08c7a581960c
Andraz Bajt <edofic@gmail.com>
2014-11-06 13:55:24 +0100

    implement state effect for issue #7

---
+++ src/Control/Effects/State.hs
@@ -0,0 +1,17 @@
+module Control.Effects.State where
+
+import Control.Effects
+import Data.Typeable
+
+newtype State s a = State (s -> (a,s)) deriving (Functor, Typeable)
+
+get :: (Member (State a) r, Typeable a) => Eff r a
+get = effect $ State $ \s -> (return s, s)
+
+put :: (Member (State s) r, Typeable s) => s -> Eff r ()
+put a = effect $ State $ \s -> (return (), a)
+
+state :: (Member (State s) r, Typeable s) => (s -> (a, s)) -> Eff r a
+state f = effect $ State $ \s -> 
+  let (a, s') = f s
+  in  (return a, s')
