5891362c0c530773ce9183a17d206af0c4927862
Andraz Bajt <edofic@gmail.com>
2014-11-13 15:04:46 +0100

    state handler

--- src/Control/Effects/Free/State.hs
+++ src/Control/Effects/Free/State.hs
@@ -1,16 +1,27 @@
 module Control.Effects.Free.State where
 
 import Control.Effects.Free.Eff
+import Control.Monad
 
-newtype State s a = State (s -> (a,s)) deriving (Functor, Typeable)
+data State s a = SGet (s -> a) 
+               | SPut s a 
+               deriving (Functor, Typeable)
 
 get :: (Member (State a) r, Typeable a) => Eff r a
-get = effect $ State $ \s -> (return s, s)
+get = effect $ SGet return
 
 put :: (Member (State s) r, Typeable s) => s -> Eff r ()
-put a = effect $ State $ \s -> (return (), a)
+put a = effect $ SPut a $ return ()
 
 state :: (Member (State s) r, Typeable s) => (s -> (a, s)) -> Eff r a
-state f = effect $ State $ \s -> 
+state f = do
+  s <- get
   let (a, s') = f s
-  in  (return a, s')
+  put s'
+  return a
+
+stateHandler :: Handler (State s) r a (s -> Eff r a) 
+stateHandler (Left a) = return $ const $ return a
+stateHandler (Right (SGet k)) = return $ \s -> join $ fmap ($s) (k s)
+stateHandler (Right (SPut s k)) = k
+  
