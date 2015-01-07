61fe727de1084973cf61e8be5557535ed15e47a8
Andraz Bajt <andraz@bajt.me>
2015-01-07 18:31:56 +0100

    eff haddock

--- src/Control/Effects/Codt/Eff.hs
+++ src/Control/Effects/Codt/Eff.hs
@@ -1,3 +1,6 @@
+-- |This implementation of the effect monad uses `Free` over a 
+-- `Union` of functors and then applies `Codensity` over it
+-- for asymptotic improvements of ill-associated binds.
 module Control.Effects.Codt.Eff
 ( Eff
 , Handler
@@ -19,25 +22,51 @@ import Control.Monad.Free
 import Data.Union
 import Data.Typeable
 
+-- |Result structure of the program is directly `Free` over `Union` 
+-- indexed by the list of effect functors.
 type Res r = Free (Union r)
 
 newtype Eff r a = Eff { runEff :: Codensity (Res r) a }
                   deriving (Functor, Applicative, Monad)
 
+-- |Handler is a function that takes a result or an effect and a continuation
+-- |and handles it. 
+-- 
+-- `e` is the effect functor you are handling
+--
+-- `r` represents the type of the type list of the remaining effects. 
+--  Usually you want to be polymorphic in this.
+--
+-- `a` is the result type of the program you will handle
+--
+-- `b` is the result of handled computation.
 type Handler e r a b = Either a (e (Res r b)) -> Res r b
 
+-- | `effect` is meant to be used as a helper function for defining new effects.
+-- See predefined effects for examples. Good way to use it is to pass in a lambda
+-- expression with explicit `k` for continuation. You will need to manually `inj` 
+-- into the `Union` because of some GHC limitations.
 effect :: (forall b . (a -> Res r b) -> Union r (Res r b)) -> Eff r a
 effect e = Eff $ Codensity $ \k -> Free $ e k
 
+-- |A program without effects is guaranteed to be pure so you 
+-- can safely convert it into a value.
 runPure :: Eff '[] a -> a
 runPure = runPureRes . finish
 
+-- |Like `runPure` but for program results. You only need this for implementing
+-- some handlers. 
 runPureRes :: Res '[] a -> a
 runPureRes (Pure a) = a
 
+-- |Finish a program and convert it into a result structure.
 finish :: Eff r a -> Res r a
 finish = lowerCodensity . runEff
 
+-- |Convert a result back into a program in order to compose it.
+-- This function might not be needed and might introduce some 
+-- performance issues (it is used in `handle`) but we didn't find 
+-- a way to drop it.
 continue :: Res r a -> Eff r a
 continue r = Eff $ Codensity (r >>=)
 
@@ -47,5 +76,10 @@ handleRes h (Free u) = case decomp u of
   Left  u -> h $ Right $ fmap (handleRes h) u
   Right u -> Free $ fmap (handleRes h) u
 
+-- |Use a `Handler` on an `Eff` program to stripe away the first layer of effects.
+-- There are some issues if you are using a handler that is somewhat polymorphic in `e`
+-- As the compiler cannot figure out which effect are you handling. Currently the best 
+-- solution seems to be to manually specify type of the handler such that it is monomorphic
+-- in `e`. Sorry.
 handle :: (Functor e, Typeable e) => Handler e r a b -> Eff (e ': r) a -> Eff r b
 handle h c = continue $ handleRes h $ finish c
--- src/Control/Effects/Cont/Eff.hs
+++ src/Control/Effects/Cont/Eff.hs
@@ -1,3 +1,6 @@
+-- |This implementation of the efffect monad uses hand rolled Free
+-- with hand rolled "inlined" codensity transformation (CPS-ish).
+-- It is supposed to be the fastest since it has no extra abstractions.
 module Control.Effects.Cont.Eff 
 ( Eff
 , Handler
@@ -19,6 +22,8 @@ import Control.Monad.Free
 import Data.Union
 import Data.Typeable
 
+-- |Result structure of the program is isomorphic to 
+-- `Free (Union r) a`
 data Res r a = Val a | E (Union r (Res r a))
              deriving Functor 
 
@@ -48,20 +53,44 @@ instance Monad (Eff r) where
             runEff m $ \a ->
             runEff (f a ) k
 
+-- |Handler is a function that takes a result or an effect and a continuation
+-- |and handles it. 
+-- 
+-- `e` is the effect functor you are handling
+--
+-- `r` represents the type of the type list of the remaining effects. 
+--  Usually you want to be polymorphic in this.
+--
+-- `a` is the result type of the program you will handle
+--
+-- `b` is the result of handled computation.
 type Handler e r a b = Either a (e (Res r b)) -> Res r b
 
+-- | `effect` is meant to be used as a helper function for defining new effects.
+-- See predefined effects for examples. Good way to use it is to pass in a lambda
+-- expression with explicit `k` for continuation. You will need to manually `inj` 
+-- into the `Union` because of some GHC limitations.
 effect :: (forall b . (a -> Res r b) -> Union r (Res r b)) -> Eff r a
 effect e = Eff $ \k -> E $ e k
 
+-- |A program without effects is guaranteed to be pure so you 
+-- can safely convert it into a value.
 runPure :: Eff '[] a -> a
 runPure = runPureRes . finish
 
+-- |Like `runPure` but for program results. You only need this for implementing
+-- some handlers. 
 runPureRes :: Res '[] a -> a
 runPureRes (Val a) = a
 
+-- |Finish a program and convert it into a result structure.
 finish :: Eff r a -> Res r a
 finish c = runEff c Val
 
+-- |Convert a result back into a program in order to compose it.
+-- This function might not be needed and might introduce some 
+-- performance issues (it is used in `handle`) but we didn't find 
+-- a way to drop it.
 continue :: Res r a -> Eff r a
 continue r = Eff (r >>=)
 
@@ -72,5 +101,10 @@ handleRes h (E u) = case decomp u of
   Left  u -> h $ Right $ fmap (handleRes h) u
   Right u -> E $ fmap (handleRes h) u
 
+-- |Use a `Handler` on an `Eff` program to stripe away the first layer of effects.
+-- There are some issues if you are using a handler that is somewhat polymorphic in `e`
+-- As the compiler cannot figure out which effect are you handling. Currently the best 
+-- solution seems to be to manually specify type of the handler such that it is monomorphic
+-- in `e`. Sorry.
 handle :: (Functor e, Typeable e) => Handler e r a b -> Eff (e ': r) a -> Eff r b
 handle h c = continue $ handleRes h $ finish c
